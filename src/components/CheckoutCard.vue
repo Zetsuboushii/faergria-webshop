0678509f999c3f96fdea1c61d7ef9a76324d4e42
Zetsuboushii <zetsuboushii@gmail.com>
2024-04-24 16:43:37 +0200

    🦆 minor changes

--- src/components/CheckoutCard.vue
+++ src/components/CheckoutCard.vue
@@ -2,13 +2,18 @@
   <v-card
     width="600"
     height="500"
-    title="Checkout"
+    title="Cart Overview"
   >
     <v-divider class="ma-5"></v-divider>
     <v-list-item v-for="item in cartItems" :key="item.item_id">
-      {{ item.item_name + " " + item.quantity }}
+      {{ item.quantity + "x " + item.item_name }}
     </v-list-item>
-    {{ total }}
+    <v-divider class="ma-5"/>
+    <div class="ma-5">
+      <p>Items: {{ (total - (total * 0.19)).toFixed(2) }}€</p>
+      <p>MwSt: {{ (total * 0.19).toFixed(2) }}€</p>
+      <p>Gesamt: {{ total }}€</p>
+    </div>
   </v-card>
 </template>
 
