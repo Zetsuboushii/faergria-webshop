12d46ee9da33bf4f9137600f8fb3983835a3623b
Zetsuboushii <zetsuboushii@gmail.com>
2024-04-30 11:46:04 +0200

    🦆 db order input

--- src/components/CheckoutCard.vue
+++ src/components/CheckoutCard.vue
@@ -1,8 +1,5 @@
 <template>
-  <v-card
-    width="600"
-    height="500"
-  >
+  <v-card class="checkout-card">
     <div class="checkout-cart">
       <h5 class="text-h5 ma-5">Cart Overview</h5>
       <v-list-item v-for="item in cartItems" :key="item.item_id">
@@ -13,39 +10,76 @@
       <p>Tax: {{ (total * 0.19).toFixed(2) }}€</p>
       <p>Total: {{ total }}€</p>
       <h5 class="text-h5 ma-5">Order Information</h5>
-      <v-sheet class="mx-auto" max-width="300">
+      <v-sheet class="mx-auto order-information">
         <v-form validate-on="submit lazy" @submit.prevent="submit">
-          <v-text-field
-            v-model="name"
-            :rules="nameRules"
-            hide-detailes="auto"
-            label="Full Name"
-          ></v-text-field>
-          <v-text-field
-            :rules="emailRules"
-            hide-detailes="auto"
-            label="E-Mail Address"
-          ></v-text-field>
-          <v-btn
-            type="submit"
-            text="Submit"
-            :loading="loading"
-          >
-          </v-btn>
+          <v-row>
+            <v-col cols="6">
+              <v-text-field
+                v-model="name"
+                :rules="nameRules"
+                hide-details="auto"
+                label="Full Name"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                hide-details="auto"
+                label="E-Mail Address"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field
+                v-model="address"
+                :rules="addressRules"
+                hide-details="auto"
+                label="Address"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="4">
+              <v-text-field
+                v-model="postal"
+                :rules="postalRules"
+                hide-details="auto"
+                label="Postal Code"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="8">
+              <v-select
+                v-model="country"
+                :items="countries"
+                :rules="countryRules"
+                label="Country"
+              ></v-select>
+            </v-col>
+            <v-col cols="12" class="text-center ">
+              <v-btn
+                type="submit"
+                text="Submit"
+              ></v-btn>
+            </v-col>
+          </v-row>
         </v-form>
+
       </v-sheet>
     </div>
   </v-card>
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
+import axios from "axios";
 
 const props = defineProps(["cartItems", "total"])
 
+const countries = ref<any[]>([])
+
 const name = ref<string>("")
 const email = ref<string>("")
-const loading = ref<boolean>(false)
+const address = ref<string>("")
+const postal = ref<string>("")
+const country = ref<string>("Select a Country")
 
 const nameRules = [
   (value: any) => !!value || "Name is required.",
@@ -54,22 +88,67 @@ const emailRules = [
   (value: any) => !!value || "E-Mail is required.",
   (value: any) => !!(/.+@.+\..+/.test(value)) || "E-Mail must be valid."
 ]
+const addressRules = [
+  (value: any) => !!value || "Address is required."
+]
+const postalRules = [
+  (value: any) => !!value || "Postal Code is required"
+]
+const countryRules = [
+  (value: any) => value !== "Select a Country" || "Country is required."
+]
+
+const fetchCountries = async () => {
+  try {
+    const res = await axios.get<any[]>('https://restcountries.com/v3.1/all')
+    countries.value = res.data.map(country => country.name.common)
+    countries.value.sort()
+  } catch (err) {
+    console.error('Error fetching countries: ', err)
+  }
+}
 
 const submit = async (event: any) => {
-  loading.value = true
+  const nameValid = nameRules.every(rule => rule(name.value) === true)
+  const emailValid = emailRules.every(rule => rule(email.value) === true)
+  const addressValid = addressRules.every(rule => rule(address.value) === true)
+  const postalValid = postalRules.every(rule => rule(postal.value) === true)
+  const countryValid = countryRules.every(rule => rule(country.value) === true)
 
-  const results = await event
+  if (!nameValid || !emailValid || !addressValid || !postalValid || !countryValid) {
+    alert("Fix")
+    return
+  }
 
-  loading.value = false
+  const orderData = {
+    name: name.value,
+    email: email.value,
+    address: address.value,
+    postal: postal.value,
+    country: country.value,
+    items: props.cartItems
+  }
 
-  alert(JSON.stringify(results, null, 2))
+  try {
+    const res = await axios.post('http://localhost:1337/save-order', orderData)
+    alert(res)
+  } catch (err) {
+    console.error('Error: ', err)
+  }
+
+  // User Feedback
 }
+
+onMounted(fetchCountries)
 </script>
 
 <style scoped>
-.checkout-cart {
+.checkout-card {
+  width: 600px;
+  height: 80vh;
   overflow: hidden;
   overflow-y: scroll;
+
 }
 
 .checkout-cart h5 {
@@ -80,5 +159,9 @@ const submit = async (event: any) => {
 
 .checkout-cart p {
   margin-left: 15px;
+}
+
+.order-information {
+  max-width: 80%;
 }
 </style>
--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ const app = express()
 const PORT = 1337
 
 app.use(cors())
-
 app.use(express.json())
 
 const db = new sqlite3.Database('./src/identifier.sqlite', sqlite3.OPEN_READWRITE, (err) => {
@@ -13,12 +12,146 @@ const db = new sqlite3.Database('./src/identifier.sqlite', sqlite3.OPEN_READWRIT
   console.log('Verbunden mit der SQLite-Datenbank.')
 })
 
+const categoriesSql = `
+  CREATE TABLE IF NOT EXISTS categories
+  (
+    category_id
+    INTEGER
+    PRIMARY
+    KEY
+    AUTOINCREMENT,
+    category_name
+    TEXT
+    NOT
+    NULL
+  );
+`;
+
+const collectionsSql = `
+  CREATE TABLE IF NOT EXISTS collections
+  (
+    collection_id
+    INTEGER
+    PRIMARY
+    KEY
+    AUTOINCREMENT,
+    collection_name
+    TEXT
+    NOT
+    NULL
+  );
+`;
+
+const itemsSql = `
+  CREATE TABLE IF NOT EXISTS items
+  (
+    item_id
+    TEXT
+    PRIMARY
+    KEY
+    NOT
+    NULL,
+    item_name
+    TEXT
+    NOT
+    NULL,
+    fk_category
+    INTEGER,
+    fk_collection
+    INTEGER,
+    price
+    REAL,
+    stock
+    INTEGER,
+    FOREIGN
+    KEY
+  (
+    fk_category
+  ) REFERENCES categories
+  (
+    category_id
+  ),
+    FOREIGN KEY
+  (
+    fk_collection
+  ) REFERENCES collections
+  (
+    collection_id
+  )
+    );
+`;
+
+const ordersSql = `
+  CREATE TABLE IF NOT EXISTS orders
+  (
+    order_id
+    INTEGER
+    PRIMARY
+    KEY
+    AUTOINCREMENT,
+    order_date
+    DATE,
+    order_email
+    TEXT,
+    order_name
+    TEXT,
+    order_address
+    TEXT,
+    order_postal
+    TEXT,
+    order_country
+    TEXT
+  );
+`;
+
+const ordersItemsSql = `
+  CREATE TABLE IF NOT EXISTS orders_items
+  (
+    fk_order_id
+    INTEGER
+    NOT
+    NULL,
+    fk_item_id
+    TEXT
+    NOT
+    NULL,
+    PRIMARY
+    KEY
+  (
+    fk_order_id,
+    fk_item_id
+  ),
+    FOREIGN KEY
+  (
+    fk_order_id
+  ) REFERENCES orders
+  (
+    order_id
+  ),
+    FOREIGN KEY
+  (
+    fk_item_id
+  ) REFERENCES items
+  (
+    item_id
+  )
+    );
+`;
+
+db.serialize(() => {
+  db.run(categoriesSql);
+  db.run(collectionsSql);
+  db.run(itemsSql);
+  db.run(ordersSql);
+  db.run(ordersItemsSql);
+});
+
 app.get('/', (req, res) => {
-  const sql = 'select * from category, item, collection, user, order'
+  const sql = 'select * from categories, item, collection, user, order'
 })
 
 app.get('/items', (req, res) => {
-  const sql = 'select * from item join category cat on cat.category_id = item.fk_category join collection col on col.collection_id = item.fk_collection ORDER BY col.collection_name, item_name'
+  const sql = 'select * from items join category cat on cat.category_id = item.fk_category join collection col on col.collection_id = item.fk_collection ORDER BY col.collection_name, item_name'
   db.all(sql, [], (err, rows) => {
     if (err) {
       res.status(400).json({error: err.message})
@@ -32,7 +165,7 @@ app.get('/items', (req, res) => {
 })
 
 app.get('/categories', (req, res) => {
-  const sql = 'select * from category ORDER BY category_name'
+  const sql = 'select * from categories ORDER BY category_name'
   db.all(sql, [], (err, rows) => {
     if (err) {
       res.status(400).json({error: err.message})
@@ -46,7 +179,7 @@ app.get('/categories', (req, res) => {
 })
 
 app.get('/collections', (req, res) => {
-  const sql = 'select * from collection ORDER BY collection_name'
+  const sql = 'select * from collections ORDER BY collection_name'
   db.all(sql, [], (err, rows) => {
     if (err) {
       res.status(400).json({error: err.message})
@@ -60,7 +193,7 @@ app.get('/collections', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-  const { username, password } = req.body
+  const {username, password} = req.body
   const sql = 'select * from user WHERE username = ? AND password = ?'
   db.get(sql, [username, password], (err, row) => {
     if (err) {
@@ -74,8 +207,8 @@ app.post('/login', (req, res) => {
   })
 })
 
-app.get('/orders', (req, res) => {
-  const sql = 'select * from order_item join main.items i on i.item_id = order_item.fk_item_id'
+app.post('/orders', (req, res) => {
+  const sql = 'select * from orders_items join main.items i on i.item_id = orders_items.fk_item_id'
   db.all(sql, [], (err, rows) => {
     if (err) {
       res.status(400).json({error: err.message})
@@ -87,6 +220,71 @@ app.get('/orders', (req, res) => {
     })
   })
 })
+
+app.post('/save-order', (req, res) => {
+  const orderData = req.body
+
+  const orderInsertSql = `
+    INSERT INTO orders (order_date, order_email, order_name, order_address, order_postal, order_country)
+    VALUES (?, ?, ?, ?, ?, ?)
+  `;
+
+  const values = [new Date(), orderData.email, orderData.name, orderData.address, orderData.postal, orderData.country];
+
+  db.run(orderInsertSql, values, function (err) {
+    if (err) {
+      console.error("Fehler beim Einfügen der Bestellung:", err.message);
+      res.status(500).json({error: err.message});
+      return;
+    }
+
+    console.log("Bestellung erfolgreich eingefügt. ID:", this.lastID);
+
+    // Jetzt können wir die Bestellungselemente einfügen
+    const orderId = this.lastID;
+    const orderItems = orderData.items;
+    console.log(orderItems)
+
+    const orderItemInsertSql = `
+      INSERT INTO orders_items (fk_order_id, fk_item_id)
+      VALUES (?, ?)
+    `;
+
+    // Eine Transaktion starten, um sicherzustellen, dass entweder alle Artikel eingefügt werden oder keiner
+    db.run("BEGIN TRANSACTION", function (err) {
+      if (err) {
+        console.error("Fehler beim Starten der Transaktion:", err.message);
+        res.status(500).json({error: err.message});
+        return;
+      }
+
+      // Artikel in die Datenbank einfügen
+      orderItems.forEach(item => {
+        db.run(orderItemInsertSql, [orderId, item.item_id], function (err) {
+          if (err) {
+            console.error("Fehler beim Einfügen des Artikels:", err.message);
+            res.status(500).json({error: err.message});
+            return;
+          }
+          console.log("Artikel erfolgreich eingefügt. ID:", this.lastID);
+        });
+      });
+
+      // Transaktion beenden
+      db.run("COMMIT", function (err) {
+        if (err) {
+          console.error("Fehler beim Beenden der Transaktion:", err.message);
+          res.status(500).json({error: err.message});
+          return;
+        }
+
+        console.log("Transaktion erfolgreich abgeschlossen.");
+        res.json({message: 'Bestellung erfolgreich gespeichert'});
+      });
+    });
+  });
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server läuft auf http://localhost:${PORT}`)
